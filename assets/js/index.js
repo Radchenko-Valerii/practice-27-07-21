775e0afd2d363959f282456ee578739c11b74ef2
Valerii Radchenko <valeravivaz@gmail.com>
2021-08-04 15:43:18 +0300

    add format text

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,4 @@
-
-function Phone (model, manufacturer, price, color, inStock) {
+function Phone(model, manufacturer, price, color, inStock) {
   this.model = model;
   this.manufacturer = manufacturer;
   this.price = price;
@@ -8,25 +7,25 @@ function Phone (model, manufacturer, price, color, inStock) {
   this.inStock = inStock;
 }
 
-
-
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; 
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function getPhones(amount) {
   const newPhones = [];
 
-  for(let i = 0; i < amount; i++) {
-    newPhones.push(new Phone (
-      `Model ${i}`, 
-      'Panasonic', 
-      getRandomInt(5000, 50000),
-      'black',
-      Math.random() >= 0.5
-    ))
+  for (let i = 0; i < amount; i++) {
+    newPhones.push(
+      new Phone(
+        `Model ${i}`,
+        "Panasonic",
+        getRandomInt(5000, 50000),
+        "black",
+        Math.random() >= 0.5
+      )
+    );
   }
 
   return newPhones;
@@ -36,54 +35,55 @@ const phones = getPhones(73);
 
 console.table(phones);
 
-
 //1-------------------------------------
 
-let j=0;
-const inStockValue = phones.forEach(function (phone){
-  if(phone.inStock === true){
-    j++
-  } else {};
+let j = 0;
+const inStockValue = phones.forEach(function (phone) {
+  if (phone.inStock) {
+    j++;
+  }
 });
 console.log(j);
 
 //2-------------------------------------
 
-const inStockPhones = phones.filter(function (phone){
+const inStockPhones = phones.filter(function (phone) {
   return phone.inStock;
 });
 
 //3-------------------------------------
 
-const inStockMapWork = inStockPhones.map(function (phone){
-  if(phone.price >= 30000){
-     phone.price = Math.round(phone.price*0.7)
-     return phone;
-  };
-})
+const inStockMapWork = inStockPhones.map(function (phone) {
+  if (phone.price >= 30000) {
+    phone.price = Math.round(phone.price * 0.7);
+    return phone;
+  }
+});
 
-const saleInStock = inStockMapWork.filter(function (phone){
+const saleInStock = inStockMapWork.filter(function (phone) {
   return phone !== undefined;
-})
-
+});
 
 // advanced ----------------------------
 
-const inStockLang = phones.map(function (obj){
-  if(obj.inStock === true){
-    obj.inStock = 'в наличии';
-  } else { obj.inStock = 'нет наличии';
+const inStockLang = phones.map(function (obj) {
+  if (obj.inStock) {
+    obj.inStock = "в наличии";
+  } else {
+    obj.inStock = "нет наличии";
   }
   return obj;
-})
+});
 
-inStockLang.forEach(function (phone){
-  console.log(`${phone.manufacturer} ${phone.model} со стоимостью ${phone.price} грн сейчас ${phone.inStock}`)
-})
+inStockLang.forEach(function (phone) {
+  console.log(
+    `${phone.manufacturer} ${phone.model} со стоимостью ${phone.price} грн сейчас ${phone.inStock}`
+  );
+});
 
 //-------------------------------------
 
-const sortFromHighPrice = phones.sort(function (a,b){
+const sortFromHighPrice = phones.sort(function (a, b) {
   if (a.price > b.price) {
     return -1;
   }
@@ -95,37 +95,65 @@ const sortFromHighPrice = phones.sort(function (a,b){
 
 //-----------------------------------
 
-const brands = ['Apple', 'Panasonic', 'Huawei', 'Salami', 'Google', 'Samsung', 'Djigit', 'Hong Wei Lang Dang Sun'];
-const color = ['Black', 'White', 'Red', 'Silver', 'DogerBlue', 'Purple', 'Gold', 'Orange']
-const model = ['one', 'A', 'X', 'Venum', 'GamePhone', 'CameraMan', 'Symbian', 'Model']
+const brands = [
+  "Apple",
+  "Panasonic",
+  "Huawei",
+  "Salami",
+  "Google",
+  "Samsung",
+  "Djigit",
+  "Hong Wei Lang Dang Sun",
+];
+const color = [
+  "Black",
+  "White",
+  "Red",
+  "Silver",
+  "DogerBlue",
+  "Purple",
+  "Gold",
+  "Orange",
+];
+const model = [
+  "one",
+  "A",
+  "X",
+  "Venum",
+  "GamePhone",
+  "CameraMan",
+  "Symbian",
+  "Model",
+];
 
-
-const randomBrand = function (){
+const randomBrand = function () {
   let i = getRandomInt(0, 8);
   return brands[i];
-}
+};
 
-const randomColor = function (){
+const randomColor = function () {
   let i = getRandomInt(0, 8);
   return color[i];
-}
+};
 
-const randomModel = function (){
+const randomModel = function () {
   let i = getRandomInt(0, 8);
   return model[i];
-}
+};
 
 function getPhonesRandom(amount) {
   const newPhones1 = [];
 
-  for(let i = 0; i < amount; i++) {
-    newPhones1.push(new Phone (
-      `${randomModel()} ${getRandomInt(1, 11)}`, 
-      randomBrand(), 
-      getRandomInt(5000, 50000),
-      randomColor(),
-      Math.random() >= 0.5
-    ))
+  for (let i = 0; i < amount; i++) {
+    newPhones1.push(
+      new Phone(
+        `${randomModel()} ${getRandomInt(1, 11)}`,
+        randomBrand(),
+        getRandomInt(5000, 50000),
+        randomColor(),
+        Math.random() >= 0.5
+      )
+    );
   }
 
   return newPhones1;
